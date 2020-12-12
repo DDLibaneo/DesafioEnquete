b1f1e1f8ecafaef025a9054fbcd80b27d00076a3
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2020-12-12 18:00:47 -0300

    Add models Poll, Option

---
+++ DesafioEnquete/Models/Option.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioEnquete.Models
+{
+    public class Option
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string OptionDescription { get; set; }
+
+        public int Votes { get; set; }
+        
+        [Required]
+        public int PollId { get; set; }
+
+        public Poll Poll { get; set; }
+    }
+}
---
+++ DesafioEnquete/Models/Poll.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioEnquete.Models
+{
+    public class Poll
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(700)]
+        public string PollDescription { get; set; }
+
+        public List<Option> Options { get; set; }
+    }
+}
