b040e7195bc0b9835377d9cb2ad3aac8e9a81100
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2020-12-12 13:10:08 -0300

    add ApplicationDbContext adn enable migrations

---
+++ DesafioEnquete/Migrations/Configuration.cs
@@ -0,0 +1,17 @@
+﻿namespace DesafioEnquete.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DesafioEnquete.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DesafioEnquete.Models.ApplicationDbContext context) { }
+    }
+}
---
+++ DesafioEnquete/Models/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace DesafioEnquete.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection") { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
