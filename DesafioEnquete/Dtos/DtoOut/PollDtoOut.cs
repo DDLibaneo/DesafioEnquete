88cf386dc96850aae369bab29ed7789f9a2a8f6c
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2020-12-12 22:11:33 -0300

    add dtos

---
+++ DesafioEnquete/Dtos/DtoOut/PollDtoOut.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioEnquete.Dtos.DtoOut
+{
+    public class PollDtoOut
+    {
+        public int Id { get; set; }
+
+        public string PollDescription { get; set; }
+
+        public List<OptionDtoOut> Options { get; set; }
+    }
+}
--- DesafioEnquete/Startup.cs
+++ DesafioEnquete/Startup.cs
@@ -24,20 +24,9 @@ namespace DesafioEnquete
 
         public IConfiguration Configuration { get; }
 
-        public Startup(IHostEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-
-            Configuration = builder.Build();
-        }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.Configure<DatabaseSettings>(Configuration.GetSection("ConnectionStrings"));
-
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
