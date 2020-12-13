19a3a57dbe0674e7f80fdb336affc47965b6ac2b
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2020-12-12 22:49:13 -0300

    Add AutoMapper

--- DesafioEnquete/Startup.cs
+++ DesafioEnquete/Startup.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using DesafioEnquete.AutoMapper;
+using DesafioEnquete.Dtos;
 using DesafioEnquete.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -31,6 +34,9 @@ namespace DesafioEnquete
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllers();
+
+            // AutoMapper config
+            services.AddAutoMapper(typeof(MappingProfile));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
